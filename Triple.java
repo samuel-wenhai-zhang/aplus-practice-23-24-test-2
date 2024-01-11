a9db04aac17db8b4b23aac8b740623ef2b9a9f54
Boltcrasher <Boltcrasher@gmail.com>
2024-01-11 13:35:27 -0600

    stuff

---
+++ Triple.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Triple {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(new File("triple.dat"));
+        int n = in.nextInt();
+        in.nextLine();
+        while (n-- > 0) {
+
+        }
+        in.close();
+    }
+}
